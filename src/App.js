bf2d167116ba4545f7adb70c7ca6538e7c660f4e
Nishanth Prabhu <nishanth.prabhu@glassbeam.com>
2022-08-08 11:13:37 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
+      <p> 
+        <Header />
+      </p>
+      
       <header className="App-header">
+        <p>
+          Portfolio Under Construction
+        </p>
+        <p>
+          <a 
+          className="App-link" 
+          href='https://github.com/nishanthprabhu2' 
+          rel="noopener noreferrer">
+          Github
+          </a>
+        </p>
+        <p>
+          <a 
+          className="App-link" 
+          href='https://www.linkedin.com/in/nishanth-prabhu26/' 
+          rel="noopener noreferrer">
+          LinkedIn
+          </a>
+        </p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <Footer />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './App.css';
+
+ const Header = () => {
+  return (
+    <header className="App">NISHANTH</header>
+  )
+}
+
+export default Header;
